bf9647d108277b65e41f7df9e863d45e8f1184a3
Cyborion <majormaciek@outlook.com>
2020-03-03 20:16:10 +0100

    Update ColumnSlider.js

--- src/ColumnSlider.js
+++ src/ColumnSlider.js
@@ -271,7 +271,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
   },
   iconContainer: {
     alignItems: 'center',
